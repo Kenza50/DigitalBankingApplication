b4906a4e6ea90e484730b65761fa8f9c57943eb7
Kenza Kharraz <Kenza.kharraz5@gmail.com>
2025-12-19 12:31:24 +0100

    push

--- compte-service/src/main/java/ma/emsi/kharraz/compteservice/repositories/ClientRepository.java
+++ compte-service/src/main/java/ma/emsi/kharraz/compteservice/repositories/ClientRepository.java
@@ -1,8 +1,9 @@
 package ma.emsi.kharraz.compteservice.repositories;
 
 import ma.emsi.kharraz.compteservice.entities.Client;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+@Configuration
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
